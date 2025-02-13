e06cd675b7fe3176f02f69f5b8f982bdbbcc43ba
skrr110 <mperacu112@gmail.com>
2025-02-13 16:59:08 +0100

    Primera Versión Analizador Sintáctico

--- src/analizador_sintactico.c
+++ src/analizador_sintactico.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <analizador_lexico.h>
-
-
 //ESTRUCTURA
 /*
 tenemos una función para iniciar (será nuestro main)
@@ -17,5 +13,51 @@ cuidado, para los delimitadores y los números la tupla solo accederá a valor
 al acabar, imprimimos que el analizador_sintactico finalizo
 
 */
+
+
+#include <stdio.h>
+#include <string.h>
+#include "analizador_lexico.h"
+
+#define TOKEN_EOF -1;
+
+
+// Función: inicia el analizador sintáctico. Usada desde el main:
+int iniciar_analizador_sintactico(){
+
+    // Variable para almacenar los componentes léxicos:
+    ComponenteLexico componenteLexico;
+    componenteLexico.token = 0;
+    componenteLexico.lexema = NULL;
+
+    // Informamos del comienzo del análisis:
+    printf("\nInicio del análisis sintáctico:\n");
+    
+    // Bucle: patrón productor - consumidor, se le pide el siguiente comp. léxico al
+    // analizador léxico.
+    do{
+        // Llamamos a la función "siguienteComponenteLexico" del analizador léxico:
+        componenteLexico = siguienteComponenteLexico();
+
+        // Imprimimos tupla <token, "lexema">,
+        printf("<%d, %s>", componenteLexico.token, componenteLexico.lexema);
+
+        /* NOTA: cuándo llegan delimitadores o números el encargado de poner
+        correctamente el formato es el analizador léxico. Para ello va a 
+        devolver como token el valor ASCII del delimitador. Los números
+        tienen un identificador, como las variables pero no son comprobados
+        en la tabla de símbolos, se pasan directamente. Los delimitadores
+        tampoco serán comprobados en la tabla de símbolos. */
+
+    }
+    while(componenteLexico.token != TOKEN_EOF);
+
+    // Informamos de que ha finalizado el analizador:
+    printf("\nAnálisis finalizado\n");
+    return 0;
+
+}
+
+
 
 
